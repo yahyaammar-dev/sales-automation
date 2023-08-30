39f1b2025c476107c100f4c44cce03f87e131f3b
shahab-edraak <shahab@edraaksystems.com>
2023-08-30 17:59:42 +0500

    hide user setting in header and change the setting icon in the side bar

--- sales/src/components/misc/Sidebar.jsx
+++ sales/src/components/misc/Sidebar.jsx
@@ -1,59 +1,66 @@
-import React from 'react'
-import Heading from './Heading'
-import Button from './Button'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import Heading from "./Heading";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const icons = [
-    '/imgs/logo.svg',
-    '/imgs/qq.png',
-    '/imgs/status.svg',
-    '/imgs/document.svg',
-    '/imgs/notification.svg',
-    '/imgs/setting-2.svg',
-    '/imgs/logout.svg'
-  ]
+    "/imgs/logo.svg",
+    "/imgs/qq.png",
+    "/imgs/status.svg",
+    "/imgs/document.svg",
+    "/imgs/notification.svg",
+    "/imgs/setting-2.svg",
+    "/imgs/logout.svg",
+    "/imgs/setting.svg",
+  ];
   const currentPath = window.location.pathname;
 
-  
   // Extract the string after the last slash (/)
-  const pathSegments = currentPath.split('/');
+  const pathSegments = currentPath.split("/");
   const lastSegment = pathSegments[pathSegments.length - 2];
 
-  var speech = false
-  var group = false
-  var setting  = false
+  var speech = false;
+  var group = false;
+  var setting = false;
 
-  console.log(window.location.origin)
+  console.log(window.location.origin);
 
-  if(pathSegments.includes('speech')){
-    speech = true
+  if (pathSegments.includes("speech")) {
+    speech = true;
   }
 
-  if(pathSegments.includes('group') ||  pathSegments.includes('group-details')  || window.location.pathname == '/' ){
-    group = true
+  if (
+    pathSegments.includes("group") ||
+    pathSegments.includes("group-details") ||
+    window.location.pathname == "/"
+  ) {
+    group = true;
   }
 
-  if(pathSegments.includes('concurrent-calls')){
-    setting = true
+  if (pathSegments.includes("concurrent-calls")) {
+    setting = true;
   }
-
 
   return (
     <div className="p-5 flex flex-col justify-between h-screen">
       <div>
         <div className="logo flex items-center gap-2">
-          <img src={icons[0]} className='w-1/3' />
-          <h1 className='sidebar__main__heading'>Sales</h1>
+          <img src={icons[0]} className="w-1/3" />
+          <h1 className="sidebar__main__heading">Sales</h1>
         </div>
-        <Heading text={'Main Menu'} />
-        <div onClick={() => {
-          navigate('/')
-        }}>
-          {
-            group  ? <Button text='Add Group' icon={icons[1]} active my /> : <Button text='Add Group' icon={icons[1]} my />
-          }
+        <Heading text={"Main Menu"} />
+        <div
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          {group ? (
+            <Button text="Add Group" icon={icons[1]} active my />
+          ) : (
+            <Button text="Add Group" icon={icons[1]} my />
+          )}
         </div>
         {/* <div onClick={() => {
           navigate('/group-details')
@@ -62,29 +69,34 @@ const Sidebar = () => {
             lastSegment == 'group-details' ? <Button text='Details' icon={icons[2]} active my /> : <Button text='Details' icon={icons[2]} my />
           }
         </div> */}
-        <div onClick={() => {
-          navigate('/speech')
-        }}>
-          {
-            speech ? <Button text='Speech' icon={icons[3]} my active /> : <Button text='Speech' icon={icons[3]} my />
-          }
+        <div
+          onClick={() => {
+            navigate("/speech");
+          }}
+        >
+          {speech ? (
+            <Button text="Speech" icon={icons[3]} my active />
+          ) : (
+            <Button text="Speech" icon={icons[3]} my />
+          )}
         </div>
-        <Heading text={'Account'} />
-        <Button text='Notification' icon={icons[4]} my />
-        <div onClick={() => {
-          navigate('/concurrent-calls')
-        }}>
-
-
-          {
-            setting ? <Button text='Setting' icon={icons[3]} my active /> : <Button text='Setting' icon={icons[3]} my />
-          }
-
+        <Heading text={"Account"} />
+        <Button text="Notification" icon={icons[4]} my />
+        <div
+          onClick={() => {
+            navigate("/concurrent-calls");
+          }}
+        >
+          {setting ? (
+            <Button text="Setting" icon={icons[icons.length - 1]} my active />
+          ) : (
+            <Button text="Setting" icon={icons[icons.length - 1]} my />
+          )}
         </div>
       </div>
-      <Button text='Logout' danger icon={icons[6]} my />
+      <Button text="Logout" danger icon={icons[6]} my />
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
