cea8de1fb895fb9aae3abbd6a0c73daf2d36df96
yahyaammar-dev <yahyaammar4807@gmail.com>
2023-09-08 18:49:48 +0500

    fixed https

--- sales-backend/app.js
+++ sales-backend/app.js
@@ -16,13 +16,13 @@ const https = require("https");
 const fs = require("fs");
 const xlsx = require('xlsx');
 
-const certificatePath = '/etc/letsencrypt/live/aivoip.org/fullchain.pem';
-const privateKeyPath = '/etc/letsencrypt/live/aivoip.org/privkey.pem';
+// const certificatePath = '/etc/letsencrypt/live/aivoip.org/fullchain.pem';
+// const privateKeyPath = '/etc/letsencrypt/live/aivoip.org/privkey.pem';
 
-const options = {
-  key: fs.readFileSync(privateKeyPath),
-  cert: fs.readFileSync(certificatePath)
-};
+// const options = {
+//   key: fs.readFileSync(privateKeyPath),
+//   cert: fs.readFileSync(certificatePath)
+// };
 
 const uploadsPath = path.join(__dirname, "uploads");
 
