f0b8cb3c9606c69a59bcb661287fef2d05ecd969
Usman Ahmed <usmanahmed@edraaksystems.com>
2023-09-10 15:49:52 +0500

    change port settings
    change port settings

--- sales-backend/app.js
+++ sales-backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
-const port = 9001;
+const port = 8000;
 // const port = 9000;
 // chaned to 9001
 const axios = require("axios");
