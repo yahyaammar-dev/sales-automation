4ccc2d445984cc95b1c74d00b04e0d3c4b688fb7
yahyaammar-dev <yahyaammar4807@gmail.com>
2023-09-16 19:22:59 +0500

    fxed question mark issue

--- sales/src/components/misc/TableDetails.jsx
+++ sales/src/components/misc/TableDetails.jsx
@@ -90,7 +90,7 @@ const TableDetail = ({ group, setGroup, transformedData, setTransfromedData, tog
       <Modal open={open} setOpen={setOpen} currentChat={currentChat} id={id} groupNumber={currentNumber} />
       <div style={{width: "100%"}} className="py-5 flex gap-5">
         <h1>Group Name:</h1>
-        <p>{group.name}</p>
+        <p>{group?.name}</p>
       </div>
       <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 bg-white">
